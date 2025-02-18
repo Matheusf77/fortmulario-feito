6d29d3d9ba1454ec6db25d580add86710a5ccbca
Matheusf77 <tiziuu244@gmail.com>
2025-02-18 16:24:43 -0300

    Add files via upload

---
+++ MATHEUS/mlly/atv.php
@@ -0,0 +1,19 @@
+<?php
+
+$produtos=[
+  "pão" => 2.50,
+  "leite" => 8.50,
+  "café" => 22.50,
+  "doce" => 5.00,
+  "chocolate" => 12.50,
+  "salgado" => 9.00,
+];
+
+$total = 0;
+
+if (isset($_POST["produtos"])){
+  echon "<h2> resumo da compra</h2><u1>";
+  
+  
+  foreach ($_POST[])
+}
---
+++ MATHEUS/mlly/matheus.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="padaria.css">
+    <title>Document</title>
+</head>
+<body>
+<div class= "quadrado">
+<h1>padaria</h1>
+<form action="atv.php" method="post">
+
+<input type="checkbox" name="pâo" >
+     <label> pão</label >
+     <label>- R$2.50<label>
+     <br><br>
+
+     <input type="checkbox" name= "leite" >
+    <label>leite</label>
+    <label>- R$8.50<label>
+    <br><br>
+
+    <input type="checkbox" name= "chocolate" >
+    <label> café </label>
+    <label>- R$22.50.<label>
+    <br><br>
+
+    <input type="checkbox" name= "doce" >
+    <label> doce </label>
+    <label>- R$5.00<label>
+    <br><br>
+
+    <input type="checkbox" name= "café" >
+    <label> choclate </label>
+    <label>- R$12.50<label>
+    <br><br>
+
+    <input type="checkbox" name= "salgado" >
+    <label> salgado </label>
+    <label>- R$9.00<label>
+    <br><br>
+
+
+    <button type= "submit" >comprar</button>
+
+  </form>
+  <div>
+</body>
+</html>
